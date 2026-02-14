f92428eeda5bd4660b9ee868147a3bdb3d15fe2e
jkky-98 <aal2525@ajou.ac.kr>
2026-02-14 19:13:47 +0900

    feat: 엔티티 스캔문제 해결

--- src/main/java/com/example/shortener/scalable_shortener/ScalableShortenerApplication.java
+++ src/main/java/com/example/shortener/scalable_shortener/ScalableShortenerApplication.java
@@ -2,8 +2,12 @@ package com.example.shortener.scalable_shortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.shortener.scalable_shortener.domain.repository")
+@EntityScan(basePackages = "com.example.shortener.scalable_shortener.domain.entity")
 public class ScalableShortenerApplication {
 
 	public static void main(String[] args) {
