e4f0db35d3f46e0df7b4c7bf629378a5164472dd
jkky-98 <aal2525@ajou.ac.kr>
2026-02-14 19:11:07 +0900

    database 설정 오류 수정

--- src/main/java/com/example/shortener/scalable_shortener/ScalableShortenerApplication.java
+++ src/main/java/com/example/shortener/scalable_shortener/ScalableShortenerApplication.java
@@ -2,9 +2,8 @@ package com.example.shortener.scalable_shortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class ScalableShortenerApplication {
 
 	public static void main(String[] args) {
