45676c0a7f481e5ad0ff1bfbfe25f5e95ef6988a
jkky-98 <aal2525@ajou.ac.kr>
2026-02-14 13:52:57 +0900

    프로젝트 세팅

---
+++ src/main/java/com/example/shortener/scalable_shortener/ScalableShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.shortener.scalable_shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalableShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalableShortenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shortener/scalable_shortener/ScalableShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shortener.scalable_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalableShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
